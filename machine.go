a8ccd5c426462a2be473a66b644e9522b5f37bdb
Hiromu OCHIAI <otiai10@gmail.com>
2018-01-10 15:01:11 +0900

    Avoid dkmachine error: "The security group name already exists"

--- machine.go
+++ machine.go
@@ -8,12 +8,14 @@ import (
 
 func (h *Handler) generateMachineOption(task *Task) (*dkmachine.CreateOptions, error) {
 
+	name := fmt.Sprintf("%s%02d", task.Prefix, task.Index)
 	opt := &dkmachine.CreateOptions{
-		Name: fmt.Sprintf("%s%02d", task.Prefix, task.Index),
+		Name: name,
 	}
 	opt.Driver = "amazonec2"
 	opt.AmazonEC2Region = "ap-southeast-2"
 	opt.AmazonEC2IAMInstanceProfile = "testtest"
+	opt.AmazonEC2SecurityGroup = name
 
 	return opt, nil
 }
