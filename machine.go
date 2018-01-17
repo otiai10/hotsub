76c56d60b417e0a1e0abffca742dafafcfd66a9f
Hiromu OCHIAI <otiai10@gmail.com>
2018-01-17 14:44:09 +0900

    Use stronger instances for star-alignment

--- machine.go
+++ machine.go
@@ -16,7 +16,9 @@ func (h *Handler) generateMachineOption(task *Task) (*dkmachine.CreateOptions, e
 
 	// opt.AmazonEC2Region = "ap-southeast-2"
 	opt.AmazonEC2Region = "ap-northeast-1"
-	opt.AmazonEC2InstanceType = "m4.xlarge"
+	// opt.AmazonEC2InstanceType = "m4.xlarge"
+	opt.AmazonEC2InstanceType = "t2.2xlarge"
+	opt.AmazonEC2RootSize = 48
 
 	opt.AmazonEC2IAMInstanceProfile = "testtest"
 	opt.AmazonEC2SecurityGroup = name
