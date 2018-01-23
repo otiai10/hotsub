4d9db50d8be05371ee4fc45aa4b8c163bc81686b
Hiromu OCHIAI <otiai10@gmail.com>
2018-01-23 16:15:40 +0900

    Respect full path of inputs/input-recursive URL
    Fix #8

--- prepare.go
+++ prepare.go
@@ -9,6 +9,10 @@ import (
 	"github.com/otiai10/daap"
 )
 
+const (
+	workdir = "/tmp"
+)
+
 // Prepare upload and locate files which are specified by the task onto the container.
 // "Prepare" lifecycle is supposed to do
 // 1) Download inputs files and directories specified by the task
@@ -51,10 +55,27 @@ func (h *Handler) Prepare(ctx context.Context, container *daap.Container, job *J
 	return nil
 }
 
+// prepareInput prepares input files to the worker container and convert it
+// to envirionment variables for the execution of user script.
+// rawurl is a raw URL with s3:// or gs:// scheme.
+// Every input file will be located on a path which is generated just by
+// replacing scheme and bucket name of rawurl with "workdir".
+// e.g.
+//  s3://hgc-otiai10-test/foo/bar.txt -> ${workdir}/foo/bar.txt
 func (h *Handler) prepareInput(ctx context.Context, c *daap.Container, envname, rawurl string, job *Job, result chan<- string) {
+
+	u, err := url.Parse(rawurl)
+	if err != nil {
+		job.Errorf("failed to parse the url of given input: %s: %v", rawurl, err)
+		result <- ""
+		return
+	}
+
+	dir := filepath.Join(workdir, filepath.Dir(u.Path))
+
 	stream, err := c.Exec(ctx, daap.Execution{
 		Inline: "/lifecycle/download.sh",
-		Env:    []string{fmt.Sprintf("%s=%s", "INPUT", rawurl), fmt.Sprintf("%s=%s", "DIR", "/tmp")},
+		Env:    []string{fmt.Sprintf("%s=%s", "INPUT", u.String()), fmt.Sprintf("%s=%s", "DIR", dir)},
 	})
 	if err != nil {
 		job.Errorf("failed to execute /lifecycle/download.sh: %v", err)
@@ -64,14 +85,31 @@ func (h *Handler) prepareInput(ctx context.Context, c *daap.Container, envname, 
 	for payload := range stream {
 		job.Logf("[PREPARE] &%d> %s", payload.Type, string(payload.Data))
 	}
-	result <- fmt.Sprintf("%s=%s", envname, filepath.Join("/tmp", filepath.Base(rawurl)))
+	result <- fmt.Sprintf("%s=%s", envname, filepath.Join(dir, filepath.Base(rawurl)))
 	return
 }
 
+// prepareInput prepares input directories to the worker container and convert it
+// to envirionment variables for the execution of user script.
+// rawurl is a raw URL with s3:// or gs:// scheme.
+// Every input file will be located on a path which is generated just by
+// replacing scheme and bucket name of rawurl with "workdir".
+// e.g.
+//  s3://hgc-otiai10-test/foo/baz -> ${workdir}/foo/baz
 func (h *Handler) prepareInputRecursive(ctx context.Context, c *daap.Container, envname, rawurl string, job *Job, result chan<- string) {
+
+	u, err := url.Parse(rawurl)
+	if err != nil {
+		job.Errorf("failed to parse the url of given input: %s: %v", rawurl, err)
+		result <- ""
+		return
+	}
+
+	dir := filepath.Join(workdir, filepath.Dir(u.Path))
+
 	stream, err := c.Exec(ctx, daap.Execution{
 		Inline: "/lifecycle/download.sh",
-		Env:    []string{fmt.Sprintf("%s=%s", "INPUT_RECURSIVE", rawurl), fmt.Sprintf("%s=%s", "DIR", "/tmp")},
+		Env:    []string{fmt.Sprintf("%s=%s", "INPUT_RECURSIVE", rawurl), fmt.Sprintf("%s=%s", "DIR", dir)},
 	})
 	if err != nil {
 		job.Errorf("failed to execute /lifecycle/download.sh: %v", err)
@@ -81,7 +119,7 @@ func (h *Handler) prepareInputRecursive(ctx context.Context, c *daap.Container, 
 	for payload := range stream {
 		job.Logf("[PREPARE] &%d> %s", payload.Type, string(payload.Data))
 	}
-	result <- fmt.Sprintf("%s=%s", envname, filepath.Join("/tmp", filepath.Base(rawurl)))
+	result <- fmt.Sprintf("%s=%s", envname, filepath.Join(dir, filepath.Base(rawurl)))
 	return
 }
 
@@ -91,7 +129,7 @@ func (h *Handler) prepareOutputDirectory(ctx context.Context, c *daap.Container,
 		result <- ""
 		return
 	}
-	outdir := filepath.Join("/tmp", u.Path)
+	outdir := filepath.Join(workdir, u.Path)
 	stream, err := c.Exec(ctx, daap.Execution{
 		Inline: fmt.Sprintf("mkdir -p %v", outdir),
 	})
