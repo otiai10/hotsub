f1107130f8064f5b5882904ea76e3e6f91a21e37
Hiromu OCHIAI <otiai10@gmail.com>
2018-01-24 16:02:38 +0900

    Fix the test which is intentionally failed

--- test/script/word-count.sh
+++ test/script/word-count.sh
@@ -7,5 +7,5 @@ PROJROOT=$(dirname $(dirname $(cd $(dirname $0) && pwd)))
 awsub \
     --tasks ${PROJROOT}/test/data/wordcount.csv \
     --script ${PROJROOT}/examples/wordcount/main.sh \
+    --aws-iam-instance-profile testtest \
     --verbose
-    # --aws-iam-instance-profile testtest \
