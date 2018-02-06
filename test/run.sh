1ea1dddf1ce6f7f61889fe678fb1b6aee60be932
Hiromu OCHIAI <otiai10@gmail.com>
2018-02-06 14:30:08 +0900

    Add test feature of selecting cases by name matching

--- test/run.sh
+++ test/run.sh
@@ -3,10 +3,28 @@
 
 PROJROOT=$(dirname $(cd $(dirname $0) && pwd))
 
+MATCH=
+while [[ $# -gt 0 ]]; do
+case "${1}" in
+  --match|-m)
+  MATCH="${2}"
+  shift && shift
+  ;;
+  *)
+  shift
+  ;;
+esac
+done
+
 for script in ${PROJROOT}/test/cases/*.sh; do
 
   # Decide expected exit status
   name=`basename ${script}`
+
+  if [[ -n ${MATCH} ]]; then
+    if [[ ${name} != *${MATCH}* ]]; then continue; fi
+  fi
+
   if [[ ${name} = *".FAIL.sh" ]]; then expected=1; else expected=0; fi
 
   # Execute testcase script
