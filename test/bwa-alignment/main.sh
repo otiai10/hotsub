17fbbe2e2f803df306e59672af72bbfe19185963
Hiromu OCHIAI <otiai10@gmail.com>
2018-04-05 12:37:54 +0900

    Make the BWA example work well!

--- test/bwa-alignment/main.sh
+++ test/bwa-alignment/main.sh
@@ -3,18 +3,18 @@
 set -e -u
 
 DEST=${OUTDIR}/${CASENAME}
+mkdir -p ${DEST}
 
+set -v
 bwa mem \
     -t 2 -T 0 \
-    ${REFRENCE}/GRCh37.fa \
+    ${REFERENCE}/GRCh37.fa \
     ${FASTQ_1} ${FASTQ_2} \
     > ${DEST}/result.sam
-
 samtools view \
     -S -b -h \
     ${DEST}/result.sam \
     > ${DEST}/result.bam
-
 samtools sort \
     ${DEST}/result.bam \
     > ${DEST}/sorted.bam
--- test/cases/bwa-alignment.sh
+++ test/cases/bwa-alignment.sh
@@ -7,6 +7,7 @@ awsub \
     --tasks ${PROJROOT}/test/bwa-alignment/bwa-alignment.csv \
     --script ${PROJROOT}/test/bwa-alignment/main.sh \
     --image otiai10/bwa \
+    --aws-ec2-instance-type t2.large \
     --aws-iam-instance-profile testtest \
     --shared REFERENCE=s3://awsub-test-otiai10/bwa-example/reference/GRCh37 \
     --verbose
