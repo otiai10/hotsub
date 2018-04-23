20a0a2bdd88e3253d2c6e682b6e7055d2cee6d7b
Hiromu OCHIAI <otiai10@gmail.com>
2018-04-23 20:16:19 +0900

    Propagate cancellation to other parallel jobs if errored

--- core/job_run.go
+++ core/job_run.go
@@ -13,10 +13,21 @@ func (job *Job) Run(ctx context.Context, shared *SharedData, sem *semaphore.Weig
 	defer close(done)
 	go job.run(shared, sem, done)
 
+	// Destroy computing instance for this job anyway,
+	// EVEN WHEN any of other job failed.
+	defer job.Destroy()
+
 	for {
 		select {
 		case err := <-done:
+			// This "err" could be nil or non-nil.
+			// Once non-nil error is returned, errgroup.WithContext would emit the cancellation
+			// to all the "job.Run" by "ctx.Done()" channel.
 			return err
+		case <-ctx.Done():
+			// If any of parallel "job.Run" has failed, the cancellation is notified by this channle.
+			// Stop, destroy and return this "job.Run" as well.
+			return ctx.Err()
 		}
 	}
 
@@ -33,8 +44,6 @@ func (job *Job) run(shared *SharedData, sem *semaphore.Weighted, done chan<- err
 		return
 	}
 	sem.Release(1)
-
-	defer job.Destroy()
 
 	if err := job.Construct(shared); err != nil {
 		done <- err
