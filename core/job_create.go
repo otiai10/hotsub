f8ebb3fa124ae467f9c7ffe5b29b5a83325884e0
Hiromu OCHIAI <otiai10@gmail.com>
2018-04-27 18:56:41 +0900

    Retry creating machine if it failed
    Related #69

--- core/job_create.go
+++ core/job_create.go
@@ -2,8 +2,14 @@ package core
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/otiai10/dkmachine/v0/dkmachine"
+)
+
+const (
+	// CreateMaxRetry represents max count for retrying `docker-machine create`
+	CreateMaxRetry = 4
 )
 
 // Create creates physical machine and wake the required containers up.
@@ -12,14 +18,33 @@ func (job *Job) Create() error {
 
 	job.Lifetime(CREATE, "Creating computing instance for this job...")
 
+	return job.create(0)
+}
+
+// create can be called recursively.
+func (job *Job) create(retry int) error {
+
+	if retry >= CreateMaxRetry {
+		return fmt.Errorf("max retry of creating machine exceeded: failed %d times", CreateMaxRetry)
+	}
+
 	spec := *job.Machine.Spec
 	job.Identity.Name = fmt.Sprintf("%s-%04d", job.Identity.Prefix, job.Identity.Index)
 	spec.Name = job.Identity.Name
 	instance, err := dkmachine.Create(&spec)
 	job.Machine.Instance = instance
-	if err != nil {
-		return err
+
+	// Succeeded!
+	if err == nil {
+		return nil
 	}
 
-	return nil
+	// Clean up for retry
+	if err := instance.Remove(); err != nil {
+		return fmt.Errorf("failed to clean up machine for retry: %v", err)
+	}
+
+	job.Lifetime(CREATE, "Retrying creating an instance for this job...")
+	time.Sleep(time.Duration(retry*40) * time.Second)
+	return job.create(retry + 1)
 }
