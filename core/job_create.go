6c378b6f6a25c784513cdb4d02569405af57e449
Hiromu OCHIAI <otiai10@gmail.com>
2018-06-04 12:22:05 +0900

    Fix bug: separate errors on Create and Remove

--- core/job_create.go
+++ core/job_create.go
@@ -35,8 +35,8 @@ func (job *Job) create(retry int, lasterror error) error {
 	}
 
 	// Clean up for retry
-	if err := instance.Remove(); err != nil {
-		return fmt.Errorf("failed to clean up machine for retry: %v", err)
+	if errOnRemove := instance.Remove(); errOnRemove != nil {
+		return fmt.Errorf("last error on create: %v: failed to clean up machine for retry: %v", err, errOnRemove)
 	}
 
 	job.Lifetime(CREATE, "Retrying creating an instance for this job...")
