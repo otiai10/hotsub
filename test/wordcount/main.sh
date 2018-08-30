98e58ab08c44da8cb3697fcec4ed1ea0c469bab3
Hiromu OCHIAI <otiai10@gmail.com>
2018-08-30 15:16:08 +0900

    Add test for independent destroying
    Related #115

--- test/wordcount/main.sh
+++ test/wordcount/main.sh
@@ -5,6 +5,7 @@ echo "HOTSUB_ROOT:  ${HOTSUB_ROOT}"
 echo "SPEAKER_NAME: ${SPEAKER_NAME}"
 echo "SPEECH_FILE:  ${SPEECH_FILE}"
 echo "OUTDIR:       ${OUTDIR}"
+echo "SLEEP:        ${SLEEP}"
 echo "======================="
 
 echo "=== Test --input-recursive ==="
@@ -23,3 +24,10 @@ cat ${SPEECH_FILE} \
   | sort -r \
   > ${OUTDIR}/word-count.txt
   # | tee ${OUTDIR}/word-count.txt
+
+if [[ ${SLEEP} -gt 1 ]]; then
+  echo "This script gonna sleep for ${SLEEP} seconds!"
+  sleep ${SLEEP}s
+fi
+
+echo "Everything done on this machine"
