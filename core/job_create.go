a972b2c3971effe079d9f94d5e26c7078e90abda
Hiromu OCHIAI <otiai10@gmail.com>
2018-08-31 19:22:00 +0900

    Retry by "RegenerateCerts" if the creation error is CertInvalid

--- core/job_create.go
+++ core/job_create.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/docker/machine/libmachine/check"
 	"github.com/otiai10/dkmachine"
 )
 
@@ -13,33 +14,41 @@ func (job *Job) Create() error {
 
 	job.Lifetime(CREATE, "Creating computing instance for this job...")
 
-	return job.create(0, nil)
+	return job.create(0, false, nil)
 }
 
 // create can be called recursively.
-func (job *Job) create(retry int, lasterror error) error {
+func (job *Job) create(retry int, regenerateCerts bool, lasterror error) error {
 
 	if retry >= CreateMaxRetry {
 		return fmt.Errorf("max retry of creating machine exceeded: failed %d times with last error: %v", CreateMaxRetry, lasterror)
 	}
 
-	spec := *job.Machine.Spec
-	job.Identity.Name = fmt.Sprintf("%s-%04d", job.Identity.Prefix, job.Identity.Index)
-	spec.Name = job.Identity.Name
-	instance, err := dkmachine.Create(&spec)
-	job.Machine.Instance = instance
+	var err error
+	if regenerateCerts {
+		err = job.Machine.Instance.RegenerateCerts()
+	} else {
+		spec := *job.Machine.Spec
+		job.Identity.Name = fmt.Sprintf("%s-%04d", job.Identity.Prefix, job.Identity.Index)
+		spec.Name = job.Identity.Name
+		job.Machine.Instance, err = dkmachine.Create(&spec)
+	}
 
 	// Succeeded!
 	if err == nil {
 		return nil
 	}
 
+	if _, ok := err.(check.ErrCertInvalid); ok {
+		return job.create(retry+1, true, err)
+	}
+
 	// Clean up for retry
-	if errOnRemove := instance.Remove(); errOnRemove != nil {
+	if errOnRemove := job.Machine.Instance.Remove(); errOnRemove != nil {
 		return fmt.Errorf("last error on create: %v: failed to clean up machine for retry: %v", err, errOnRemove)
 	}
 
 	job.Lifetime(CREATE, "Retrying instance creation for this job...")
 	time.Sleep(time.Duration(retry*40) * time.Second)
-	return job.create(retry+1, err)
+	return job.create(retry+1, false, err)
 }
