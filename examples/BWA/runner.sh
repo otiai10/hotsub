e38a859e46cd8f4711386ad756529bd7dab05cd0
Hiromu OCHIAI <otiai10@gmail.com>
2018-10-24 17:59:18 +0900

    Move examples to "showcase" project

--- examples/BWA/runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -v
-hotsub \
-    --tasks ./bwa-alignment.csv \
-    --script ./main.sh \
-    --image otiai10/bwa \
-    --aws-ec2-instance-type t2.large \
-    --verbose
--- examples/helloworld/hello.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo "Hello! My name is ${NAME}."
